25b0d97534908a3830208221504337dfe0e73c17
C Shi <icydoge@gmail.com>
2019-06-27 21:18:51 +0100

    Revise script

--- setup.sh
+++ setup.sh
@@ -9,17 +9,30 @@ WLAN_IFACE="WLAN_INTERFACE_ID"
 ENDPOINT_HOST="WIREGUARD_REMOTE_HOST"
 ENDPOINT_PORT="WIREGUARD_REMOTE_PORT"
 
+echo "net.ipv4.ip_forward=1" >> /etc/sysctl.conf
+sysctl -p
+
 apt install dnsmasq hostapd -y
+
+mv /etc/dnsmasq.conf /etc/dnsmasq.conf.orig
+touch /etc/dnsmasq.conf 
+echo "interface=wlan0" >> /etc/dnsmasq.conf
+echo "dhcp-range=192.168.100.2,192.168.100.100,255.255.255.0,24h" >> /etc/dnsmasq.conf
+systemctl reload dnsmasq
+systemctl enable dnsmasq
 
 echo "interface wlan0" >> /etc/dhcpcd.conf
 echo "    static ip_address=192.168.100.1/24" >> /etc/dhcpcd.conf
 echo "    nohook wpa_supplicant" >> /etc/dhcpcd.conf
+systemctl restart dhcpcd
 
 echo DAEMON_CONF=\"/etc/hostapd/hostapd.conf\" >> /etc/default/hostapd
-
 sed -i -e "s/<AP_SSID>/$AP_SSID/g" etc/hostapd/hostapd.conf
 sed -i -e "s/<AP_PASSWORD>/$AP_PASSWORD/g" etc/hostapd/hostapd.conf
 cp etc/hostapd/hostapd.conf /etc/hostapd/hostapd.conf
+systemctl unmask hostapd
+systemctl enable hostapd
+systemctl start hostapd
 
 apt install raspberrypi-kernel-headers -y
 echo "deb http://deb.debian.org/debian/ unstable main" | sudo tee --append /etc/apt/sources.list.d/unstable.list
@@ -37,19 +50,5 @@ sed -i -e "s/<WLAN_IFACE>/$WLAN_IFACE/g" etc/wireguard/pinet.conf
 sed -i -e "s/<ENDPOINT_HOST>/$ENDPOINT_HOST/g" etc/wireguard/pinet.conf
 sed -i -e "s/<ENDPOINT_PORT>/$ENDPOINT_PORT/g" etc/wireguard/pinet.conf
 cp etc/wireguard/pinet.conf /etc/wireguard/pinet.conf
-
-echo "net.ipv4.ip_forward=1" >> /etc/sysctl.conf
-sysctl -p
-
-systemctl daemon-reload
-
-systemctl unmask hostapd
-systemctl enable hostapd
-systemctl enable dnsmasq
-systemctl enable dhcpcd
 systemctl enable wg-quick@pinet
-
-systemctl restart hostapd
-systemctl restart dnsmasq
-systemctl restart dhcpcd
 systemctl restart wg-quick@pinet
